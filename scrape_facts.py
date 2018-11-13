d53d830d412a7e96dfcb36f8d33e009a323e6aa1
tghimself <34167018+tghimself@users.noreply.github.com>
2018-11-14 00:06:43 +0530

    Update scrape_facts.py

--- scrape_facts.py
+++ scrape_facts.py
@@ -23,11 +23,11 @@ def parse(url):
     return [_.text for _ in soup.findAll("div",{"class":"fact_text"})]
     
 
-page = requests.get('https://funfactz.com/tags/')
+page = requests.get('https://funfactz.com/tags/') # we use this website to scrape all of the facts
 page.raise_for_status()
 soup = BeautifulSoup(page.content, 'html.parser')
 
-topics_temp = soup.findAll('ul',{'class':"topics cf"})
+topics_temp = soup.findAll('ul',{'class':"topics cf"}) 
 
 topics_temp = [_.findChildren("li") for _ in topics_temp]
 
@@ -36,29 +36,28 @@ topics = []
 for _ in topics_temp:
     topics.extend(_)
 
-counts = [int(i.span.text[1:-1])//8+1 for i in topics]
+counts = [int(i.span.text[1:-1])//8+1 for i in topics] # the count of facts in each topic and then divide it by 8 as each page has atmost 8 facts
 print("topics saved...")
+
 url_data = {}
-
 for _ in topics:
-    url_data[_.a.get("title")] = _.a.get("href")
+    url_data[_.a.get("title")] = _.a.get("href") 
 
-data = {} # shelve.open("data")
+data = shelve.open("data", writeback = True)
 items_left=len(url_data)
-i=0
+i=0                                                     # to run counts list with the dict 
 for title,url  in url_data.items():
     print("saving for title \"" + str(title)+"\"")
     for count in range(counts[i]):
-        if count!=0:
+        if count!=0:                                    # http://url.com/tag/topic/1 is not a valid url, 
             new_url = url+str(count+1)
         else:
-            new_url = url
-        data.setdefault(title.casefold(),[])
-        data[title.casefold()].extend(parse(new_url))
+            new_url = url                               # http://url.com/tag/topic/2 is a valid url
+        data.setdefault(title.casefold(),[])            # save a list by default
+        data[title.casefold()].extend(parse(new_url))   # extend the parsed list into this list
         print("saved: ", len(data[title.casefold()]))
-        # print(new_url, parse(new_url))
-    i+=1
-    print("Progress: {:.3f}%".format(i/items_left*100))
+    i+=1                                                # increment to keep counts list in sync with the loop
+    print("Progress: {:.3f}%".format(i/items_left*100)) # progress
 
 data.close()
 
