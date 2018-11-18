0b1dd6c9dcf8957f8e13f95cd2fa5a49e67f951e
Murtaza Nazir <34167018+themurtazanazir@users.noreply.github.com>
2018-11-19 00:50:39 +0530

    Add files via upload

--- master/twitter_bot.py
+++ master/twitter_bot.py
@@ -1,7 +1,8 @@
 import tweepy
 import time
 import shelve
-import random
+import string
+import nltk
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
@@ -24,26 +25,40 @@ def store_last_seen_id(last_seen_id,filename):
         file.write(str(last_seen_id))
     return
 
+def tagify(str1):
+    str1= str1.casefold()                                                       # for comparison
+    if "ain't" in str1:                                                         # tokenize didn't seem to remove this one
+        str1=str1.replace("ain't", "")
+    puncts = string.punctuation
+    tags = nltk.word_tokenize(str1)                                             # split the string well
+    stopwords = nltk.corpus.stopwords.words("english")
+    return set([word for word in tags if word not in list(puncts) + stopwords+["'s", "n't"]])   #return a  set of tags
+
+def get_random_fact(username):
+    with shelve.open('.\\scraping\\database') as db:                            # open the shelve file
+        fact = random.choice(list(db.keys()))
+        if len(fact) + len('@' + username) > 260:
+            return get_random_fact(username)
+        print("responding with a random fact")
+        return fact
 
 
-def get_random_fact(username): # improve this function
-    '''returns a tweet of validfor a particular username'''
-    with shelve.open('..\\scraping\\data') as fact_dict:                        # open the shelve file
-        random_id = len(fact_dict.keys())                                       # store the number of topics present
-        random_id = random.randint(0,random_id-1)                               # generate a random number b/w 0 and highest index a topic can have
-        topic = list(fact_dict.keys())[random_id]                               # access the topic at the random index
-        print("responding with a fact from", topic)       
-        random_id = len(fact_dict[topic])                                       #  store the number of facts present in that topic
-        random_id = random.randint(0,random_id-1)                               # generate a random number b/w 0 and highest index a fact can have
-        count = 0                                                               # set counter for number of attempts
-        while len(fact_dict[topic][random_id]) + len('@'+username) > 260:       # if fact is above tweet limit
-            random_id = random.randint(0,random_id-1)                           # generate a new fact
-            count+=1                                                            # increment the number of attempts
-            if count > 50:                                                      # if attempts > 50, topic doesn't have any fact of tweet limit
-                break                                                           # stop loop
-        else:                                                                   
-            return fact_dict[topic][random_id]                                  # if break does not occur, return fact
-        return get_random_fact(username)                                        # else repeat process
+def get_relevant_fact(tweet,username):                                          # takes username and tweet
+    with shelve.open('.\\scraping\\database') as db:
+            tweet = tagify(tweet)                                               # get the set of tags
+            maximum = 0                                                         # set the maximum of common tags to zero
+            fact = ""
+            fact_list = []                                                      # it will contain the facts which have the maximum common tags
+            for i in db.keys():
+                if len(tweet.intersection(db[i])) > maximum:                    # maximize the maximum
+                    maximum = len(tweet.intersection(db[i]))
+                    fact_list.clear()                                           # since we found the new maximum, clear the list
+                    fact_list.append(i)                                         # and add the new fact to the list
+                elif len(tweet.intersection(db[i])) == maximum:                 
+                    fact_list.append(i)                                         # also add any other fact which has same common tags
+            if fact == "":
+                return get_random_fact(username)                                # if there was no intersection, then fetch a random tweet
+            return random.choice(fact_list)                                     # else just choose a random fact from the list
 
 
 
@@ -57,7 +72,7 @@ def reply():
         print(mention.id,mention.full_text)
         last_seen_id = mention.id
         store_last_seen_id(last_seen_id, FILE_NAME)
-        fact = get_random_fact(mention.user.screen_name)
+        fact = get_relevant_fact(mention.full_text,mention.user.screen_name)
         api.update_status('@' + mention.user.screen_name + ' Did you know? \"' + fact + '"', mention.id)
 
 while True:
